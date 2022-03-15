7409dc278a3d8e4ac1c7c63c138241200904800d
simeddk <65766852+simeddk@users.noreply.github.com>
2022-03-15 21:05:23 +0900

    0315_CameraSpline

--- U03_Game/Source/U03_Game/Cameras/CCameraActor.cpp
+++ U03_Game/Source/U03_Game/Cameras/CCameraActor.cpp
@@ -1,20 +1,84 @@
 #include "CCameraActor.h"
+#include "Global.h"
+#include "CCameraSpline.h"
+#include "Components/SplineComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
 
 ACCameraActor::ACCameraActor()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	CHelpers::CreateComponent(this, &Camera, "Camera");
 }
+
+
 
 void ACCameraActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	TArray<AActor*> actors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACCameraSpline::StaticClass(), actors);
+	CheckFalse(actors.Num() > 0);
+
+	for (AActor* actor : actors)
+	{
+		if (actor->IsA<ACCameraSpline>() && actor->GetName().Contains("BP_CCameraSpline"))
+			Spline = Cast<ACCameraSpline>(actor);
+	}
 }
 
 void ACCameraActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (Timeline.IsPlaying())
+		Timeline.TickTimeline(DeltaTime);
 }
 
+void ACCameraActor::StartTimeline()
+{
+	CheckTrue(Timeline.IsPlaying());
+
+	FOnTimelineFloat progress;
+	progress.BindUFunction(this, "OnProgress");
+
+	FOnTimelineEvent finish;
+	finish.BindUFunction(this, "OnFinishProgress");
+
+	Timeline = FTimeline();
+	Timeline.AddInterpFloat(Spline->GetCurve(), progress);
+	Timeline.SetTimelineFinishedFunc(finish);
+	Timeline.SetTimelineLengthMode(ETimelineLengthMode::TL_LastKeyFrame);
+	Timeline.SetPlayRate(0.25f);
+	Timeline.PlayFromStart();
+
+	APlayerController* controller = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	CheckNull(controller);
+	controller->SetViewTarget(this);
+}
+
+void ACCameraActor::OnProgress(float Output)
+{
+	USplineComponent* spline = Spline->GetSpline();
+	float length = spline->GetSplineLength();
+
+	FVector location = spline->GetLocationAtDistanceAlongSpline(Output * length, ESplineCoordinateSpace::World);
+	FRotator rotation = spline->GetRotationAtDistanceAlongSpline(Output * length, ESplineCoordinateSpace::World);
+
+	SetActorLocation(location);
+	SetActorRotation(rotation);
+}
+
+void ACCameraActor::OnFinishProgress()
+{
+	Timeline.Stop();
+
+	APlayerController* controller =	UGameplayStatics::GetPlayerController(GetWorld(), 0); 
+	CheckNull(controller);
+
+	ACharacter* player = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+	CheckNull(player);
+	controller->SetViewTarget(Cast<AActor>(player));
+}
--- U03_Game/Source/U03_Game/Cameras/CCameraActor.h
+++ U03_Game/Source/U03_Game/Cameras/CCameraActor.h
@@ -9,9 +9,24 @@ UCLASS()
 class U03_GAME_API ACCameraActor : public AActor
 {
 	GENERATED_BODY()
+
+private:
+	UPROPERTY(VisibleDefaultsOnly)
+		class UCameraComponent* Camera;
 	
 public:	
 	ACCameraActor();
+
+public:
+	UFUNCTION(BlueprintCallable)
+		void StartTimeline();
+
+private:
+	UFUNCTION()
+		void OnProgress(float Output);
+
+	UFUNCTION(BlueprintCallable)
+		void OnFinishProgress();
 
 protected:
 	virtual void BeginPlay() override;
@@ -21,5 +36,5 @@ public:
 
 private:
 	FTimeline Timeline;
-
+	class ACCameraSpline* Spline;
 };
