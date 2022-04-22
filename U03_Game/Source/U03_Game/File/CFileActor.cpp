d80b760576ef1be422569eaa787e0592dcf92201
simeddk <65766852+simeddk@users.noreply.github.com>
2022-04-22 21:21:02 +0900

    0422_FileArchive 끝

--- U03_Game/Source/U03_Game/File/CFileActor.cpp
+++ U03_Game/Source/U03_Game/File/CFileActor.cpp
@@ -1,5 +1,6 @@
 #include "CFileActor.h"
 #include "Global.h"
+#include "Serialization/BufferArchive.h"
 
 ACFileActor::ACFileActor()
 {
@@ -14,7 +15,53 @@ void ACFileActor::BeginPlay()
 	CheckNull(controller);
 	EnableInput(controller);
 	
-	//TODO
+	InputComponent->BindAction("SaveFile", EInputEvent::IE_Pressed, this, &ACFileActor::OnSaveFile);
+	InputComponent->BindAction("LoadFile", EInputEvent::IE_Pressed, this, &ACFileActor::OnLoadFile);
 }
 
 
+void ACFileActor::OnSaveFile()
+{
+	FDataStruct data;
+	data.ID = "someddk";
+	data.Key = 777;
+
+	FBufferArchive buffer;
+	//buffer << data.ID;
+	//buffer << data.Key;
+	buffer << data;
+
+	CheckTrue(buffer.Num() < 1);
+	buffer.FlushCache();
+
+	FString path = FPaths::ProjectDir() + FString("Test.bin");
+	bool b = FFileHelper::SaveArrayToFile(buffer, *path);
+
+	buffer.Empty();
+
+	CLog::Print(b ? "Save Succeed" : "Save Failed");
+
+	buffer.Close();
+}
+
+void ACFileActor::OnLoadFile()
+{
+	FString path = FPaths::ProjectDir() + FString("Test.bin");
+	
+	FBufferArchive buffer;
+	FFileHelper::LoadFileToArray(buffer, *path);
+
+	FMemoryReader reader = FMemoryReader(buffer, true);
+	reader.Seek(0);
+
+	FDataStruct data;
+	//reader << data.ID;
+	//reader << data.Key;
+	reader << data;
+
+	CLog::Print(data.ID.ToString(), -1, 10.0f, FColor::Green);
+	CLog::Print((int32)data.Key, -1, 10.0f, FColor::Green);
+
+	reader.FlushCache();
+	reader.Close();
+}
--- U03_Game/Source/U03_Game/File/CFileActor.h
+++ U03_Game/Source/U03_Game/File/CFileActor.h
@@ -4,6 +4,24 @@
 #include "GameFramework/Actor.h"
 #include "CFileActor.generated.h"
 
+USTRUCT(BlueprintType)
+struct FDataStruct
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+		FName ID;
+
+	UPROPERTY(EditAnywhere)
+		uint32 Key;
+
+	friend FArchive& operator<<(FArchive& InArchive, FDataStruct& InData)
+	{
+		return InArchive << InData.ID << InData.Key;
+	}
+};
+
 UCLASS()
 class U03_GAME_API ACFileActor : public AActor
 {
@@ -15,5 +33,8 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+private:
+	void OnSaveFile();
+	void OnLoadFile();
 
 };
