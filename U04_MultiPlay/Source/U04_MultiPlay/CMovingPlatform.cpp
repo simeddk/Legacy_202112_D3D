229efb4646816b5af687d35c048fe9665c33435e
simeddk <65766852+simeddk@users.noreply.github.com>
2022-04-27 21:56:17 +0900

    0427_ServerTest

--- U04_MultiPlay/Source/U04_MultiPlay/CMovingPlatform.cpp
+++ U04_MultiPlay/Source/U04_MultiPlay/CMovingPlatform.cpp
@@ -5,6 +5,14 @@ ACMovingPlatform::ACMovingPlatform()
 	PrimaryActorTick.bCanEverTick = true;
 
 	SetMobility(EComponentMobility::Movable);
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> mesh(TEXT("/Game/Geometry/Meshes/1M_Cube"));
+	if (mesh.Succeeded())
+		GetStaticMeshComponent()->SetStaticMesh(mesh.Object);
+
+	ConstructorHelpers::FObjectFinder<UMaterial> material(TEXT("/Game/Materials/MAT_Platform"));
+	if (material.Succeeded())
+		GetStaticMeshComponent()->SetMaterial(0, material.Object);
 }
 
 void ACMovingPlatform::BeginPlay()
